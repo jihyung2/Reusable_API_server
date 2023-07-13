37d685e8eb1fd9403ad2641ac89b987253d2ef50
jihyung2 <wlgud1116@naver.com>
2023-07-14 02:39:45 +0900

    java spring boot를 활용한 프론트와 백 사이의 api 서버 구축 7/13

--- Reusable_API_Server/src/main/java/com/example/reusable_api_server/controller/ReusableApiServer.java
+++ Reusable_API_Server/src/main/java/com/example/reusable_api_server/controller/ReusableApiServer.java
@@ -37,16 +37,15 @@ public class ReusableApiServer {
 //                .bodyToMono(String.class);
 //    }
 
-    @GetMapping("/getData/{type}") //GET 요청에 대한 핸들러 메서드입니다. /api/getData 경로로 들어오는 GET 요청을 처리합니다.
-    public Mono<String> getDataFromBackend2(@PathVariable String type, @RequestBody HashMap<String, Object> map) { //리액티브 프로그래밍을 위한 Mono 클래스로, 비동기적으로 처리되는 문자열을 나타냅니다.
+    @GetMapping("/getData") //GET 요청에 대한 핸들러 메서드입니다. /api/getData 경로로 들어오는 GET 요청을 처리합니다.
+    public Mono<String> getDataFromBackend2(@RequestBody HashMap<String, Object> map) { //리액티브 프로그래밍을 위한 Mono 클래스로, 비동기적으로 처리되는 문자열을 나타냅니다.
 
-
-        if (type.equals("test")){
-            System.out.println("성공입니다.");
-        }
         System.out.println(map.get("type"));
 
-        String backendUrl = apiUrl+type;
+        String datatype = (String) map.get("type");
+
+        String backendUrl = apiUrl+datatype;
+
         //스프링 5부터 도입된 비동기 HTTP 통신을 위한 클라이언트입니다. 다양한 HTTP 요청을 보내고 응답을 받을 수 있습니다.
         WebClient webClient = WebClient.create(); //WebClient 인스턴스를 생성하는 메서드입니다.
 
